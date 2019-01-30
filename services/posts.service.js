ea65a4f7d199cd86e015abca5b320c6d581f9a88
Rishi Ghan <hghan@apple.com>
2019-01-30 15:22:14 -0800

    Committing broken code

--- services/posts.service.js
+++ services/posts.service.js
@@ -5,8 +5,27 @@ module.exports = {
 	version: 1,
 	mixins: [ DBService("posts") ],
 	actions: {
-		getPosts() {
-			return {foo: "boo"};
+		retrieve:
+			{
+				cache: 
+				{
+					keys: ["title", "excerpt", "slug", "content"]
+				},
+				params: {
+					post: { type: "string" }
+				}
+			}
+	},
+
+	settings: {
+		fields: ["_id", "slug", "tags", "date_created", "date_updated", "attachment", "is_sticky", "is_archived", "is_draft", "content", "excerpt"],
+		entityValidator: {
+			title: {type: "string", min: 1},
+			slug: {type: "string", optional: true},
+			content: {type: "string", optional: true},
+			attachment: {type: "string", items: "object", optional: true},
+			tags: {type: "array", items: "string", optional: true},
+			excerpt: {type: "string", optional: true}
 		}
 	}
 };
