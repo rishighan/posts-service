b0f767589a6473144d280b96663429f5d93c76e5
Rishi Ghan <rishi@Rishis-Mac-mini.local>
2019-10-10 06:51:23 -0700

    Changes

--- services/posts.service.js
+++ services/posts.service.js
@@ -5,7 +5,7 @@ const _ = require('lodash');
 module.exports = {
 	name: "posts",
 	version: 1,
-	mixins: [ DBService("posts") ],
+	mixins: [DBService("posts")],
 	actions: {
 		create: {
 			cache: {
@@ -14,7 +14,7 @@ module.exports = {
 			params: {
 				title: { type: "string" },
 				slug: { type: "string" },
-				tags: { type: "array", optional: true },  
+				tags: { type: "array", optional: true },
 				attachment: { type: "array", optional: true },
 				is_draft: { type: "boolean" },
 				is_sticky: { type: "boolean" }, // <- TODO
@@ -29,7 +29,7 @@ module.exports = {
 				}, broker.params);
 				return new Promise((resolve, reject) => {
 					Post.create(postData, (error, data) => {
-						if(data) {
+						if (data) {
 							resolve(data);
 						} else if (error) {
 							reject(new Error(error));
@@ -39,19 +39,18 @@ module.exports = {
 			}
 
 		},
-		retrieve:{
+		retrieve: {
 			cache: {
 				keys: ["title", "excerpt", "slug", "content"]
 			},
 			params: {
 				_id: { type: "string", optional: true },
-				slug: { type: "string", optional: true},
+				slug: { type: "string", optional: true },
 				title: { type: "string", optional: true },
-				pageOffset: { type: "string", optional: true},
-				pageLimit: { type: "string", optional: true},
+				pageOffset: { type: "string", optional: true },
+				pageLimit: { type: "string", optional: true },
 			},
 			handler(broker) {
-				console.log(broker.params);
 				const queryConfig = {
 					_id: broker.params._id,
 					slug: broker.params.slug,
@@ -65,15 +64,16 @@ module.exports = {
 					limit: parseInt(broker.params.pageLimit, 10),
 					page: parseInt(broker.params.pageOffset, 10),
 				};
+				console.log(query)
 				return new Promise((resolve, reject) => {
 					return Post.paginate(query, pagingOptions, (error, resultSet) => {
-						if(resultSet) {
+						if (resultSet) {
 							resolve(resultSet);
-						} else if(error) {
+						} else if (error) {
 							reject(new Error(error));
 						}
 					});
-				}); 
+				});
 			}
 		},
 		findByTagName: {
@@ -86,26 +86,26 @@ module.exports = {
 				pageLimit: { type: "string", optional: true }
 			},
 			handler(broker) {
-				let options = {
+				let pagingOptions = {
 					sort: { date_updated: -1 },
-					page: parseInt(broker.params.pageOffset, 10) || 1,
-					limit: parseInt(broker.params.pageLimit, 10) || 5
+					page: parseInt(broker.params.pageOffset, 10) || 0, 
+					limit: parseInt(broker.params.pageLimit, 10) || Infinity,
 				};
 				let query = { tags: { $elemMatch: { id: broker.params.tagName } }, is_draft: false, is_archived: false };
 				return new Promise((resolve, reject) => {
-					return Post.paginate(query, options, (error, resultSet) => {
-						if(resultSet){
+					return Post.paginate(query, pagingOptions, (error, resultSet) => {
+						if (resultSet) {
 							resolve(resultSet.docs);
-						} else if(error) {
+						} else if (error) {
 							reject(new Error(error));
 						}
 					});
 				});
-			}	
+			}
 		},
 		filterPostsByTags: {
 			cache: {
-				keys: [ "title", "slug", "content" ]
+				keys: ["title", "slug", "content"]
 			},
 			params: {
 				tagNames: { type: "array" }
@@ -118,7 +118,7 @@ module.exports = {
 		},
 		searchPosts: {
 			cache: {
-				keys: [ "title", "excerpt" ]
+				keys: ["title", "excerpt"]
 			},
 			params: {
 				searchTerm: { type: "string" },
@@ -130,12 +130,12 @@ module.exports = {
 					page: parseInt(broker.params.pageOffset, 10),
 					limit: parseInt(broker.params.pageLimit, 10)
 				};
-				let query = { $text: { $search: broker.params.searchTerm }};
+				let query = { $text: { $search: broker.params.searchTerm } };
 				return new Promise((resolve, reject) => {
 					return Post.paginate(query, options, (error, resultSet) => {
-						if(resultSet) {
+						if (resultSet) {
 							resolve(resultSet);
-						} else if(error) {
+						} else if (error) {
 							reject(new Error(error));
 						}
 					});
@@ -168,20 +168,20 @@ module.exports = {
 								}
 							}
 						}],
-					(err, data) => {
-						if (err) {
-							reject(err);
+						(err, data) => {
+							if (err) {
+								reject(err);
+							}
+							resolve(data);
 						}
-						resolve(data);
-					}
-					); 
+					);
 				});
 			}
 		},
-		
+
 		getStatistics: {
 			cache: {
-				keys: [ "title", "excerpt"]
+				keys: ["title", "excerpt"]
 			},
 			handler(broker) {
 				let drafts = broker.call("v1.posts.count", { query: { is_draft: true } });
@@ -193,10 +193,10 @@ module.exports = {
 							drafts: data[0],
 							blogPosts: data[2],
 							total: data[1]
-						};	
+						};
 					})
-					.catch((error) => error )
-					.finally((response) => response);								
+					.catch((error) => error)
+					.finally((response) => response);
 			}
 		},
 		update: {
@@ -206,7 +206,7 @@ module.exports = {
 			params: {
 				title: { type: "string" },
 				slug: { type: "string" },
-				tags: { type: "array", optional: true },  
+				tags: { type: "array", optional: true },
 				date_created: { type: "string" },
 				attachment: { type: "array", optional: true },
 				is_draft: { type: "boolean" },
@@ -214,38 +214,38 @@ module.exports = {
 				is_archived: { type: "boolean" },
 				content: { type: "string", optional: true },
 				excerpt: { type: "string" },
-				upsertValue: {type: "boolean" }
+				upsertValue: { type: "boolean" }
 			},
 			handler(broker) {
 				return new Promise((resolve, reject) => {
 					return Post.updateOne({
 						_id: broker.params.postId
-					}, 
-					{
-						$set: 
+					},
 						{
-							title: broker.params.title,
-							slug: broker.params.slug,
-							tags: broker.params.tags,
-							date_created: broker.params.date_created,
-							date_updated: new Date(),
-							attachment: broker.params.attachment,
-							is_draft: broker.params.is_draft,
-							is_archived: broker.params.is_archived,
-							content: broker.params.content,
-							excerpt: broker.params.excerpt
-						}
-					},
-					{ 
-						upsert: broker.params.upsertValue 
-					},
-					(error, data) => {
-						if(data) {
-							resolve(data);
-						} else if(error) {
-							reject(new Error(error));
-						}
-					});
+							$set:
+							{
+								title: broker.params.title,
+								slug: broker.params.slug,
+								tags: broker.params.tags,
+								date_created: broker.params.date_created,
+								date_updated: new Date(),
+								attachment: broker.params.attachment,
+								is_draft: broker.params.is_draft,
+								is_archived: broker.params.is_archived,
+								content: broker.params.content,
+								excerpt: broker.params.excerpt
+							}
+						},
+						{
+							upsert: broker.params.upsertValue
+						},
+						(error, data) => {
+							if (data) {
+								resolve(data);
+							} else if (error) {
+								reject(new Error(error));
+							}
+						});
 				});
 			}
 		},
@@ -257,9 +257,9 @@ module.exports = {
 			handler(broker) {
 				return new Promise((resolve, reject) => {
 					return Post.findByIdAndDelete(broker.params.postId, (error, data) => {
-						if(data) {
+						if (data) {
 							resolve(data);
-						} else if(error) {
+						} else if (error) {
 							reject(new Error(error));
 						}
 					});
@@ -271,12 +271,12 @@ module.exports = {
 		model: Post,
 		fields: ["_id", "slug", "tags", "date_created", "date_updated", "attachment", "is_sticky", "is_archived", "is_draft", "content", "excerpt"],
 		entityValidator: {
-			title: {type: "string", min: 1},
-			slug: {type: "string", optional: true},
-			content: {type: "string", optional: true},
-			attachment: {type: "string", items: "object", optional: true},
-			tags: {type: "array", items: "string", optional: true},
-			excerpt: {type: "string", optional: true}
+			title: { type: "string", min: 1 },
+			slug: { type: "string", optional: true },
+			content: { type: "string", optional: true },
+			attachment: { type: "string", items: "object", optional: true },
+			tags: { type: "array", items: "string", optional: true },
+			excerpt: { type: "string", optional: true }
 		}
 	}
 };
